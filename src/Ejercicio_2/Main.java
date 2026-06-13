1988b3820d0b3dad08a1d1a32d9a965368a04a1c
DionicioPujols <dionicio_hernandez@ucne.edu.do>
2026-06-13 18:17:13 -0400

    Modificacion al ejercicio_2

--- src/Ejercicio_2/Main.java
+++ src/Ejercicio_2/Main.java
@@ -1,16 +1,29 @@
+
+
 public class Main {
     public static void main(String[] args){
+       
+       try{
         System.out.println("|| -- Programa para simular una carrera entre corredores -- ||\n");
         MiHilo Corredor1 = new MiHilo("Juan");
         MiHilo Corredor2 = new MiHilo("Pedro");
         MiHilo Corredor3 = new MiHilo("Ana");
         MiHilo Corredor4 = new MiHilo("Carlos");
 
+        System.out.println("|| -- Estado del Hilo -- ||\n");
+        System.out.println("Estado del hilo: " + Corredor1.getState());
+        System.out.println("Estado del hilo: " + Corredor1.getState());
+        System.out.println("Estado del hilo: " + Corredor1.getState());
+        System.out.println("Estado del hilo: " + Corredor1.getState());
+        System.out.println("\n|| -- Comenzo la carrera -- ||\n");
         Corredor1.start();
         Corredor2.start();
         Corredor3.start();
         Corredor4.start();
 
-        System.out.println("Estado inicial: " + MiHilo.currentThread().getState());
+       }catch(Exception e){
+        e.printStackTrace();
+       }
+
     }
 }
