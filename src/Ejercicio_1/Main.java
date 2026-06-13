644082fcfa2414a89ea34f36599ed9bddef89e49
DionicioPujols <dionicio_hernandez@ucne.edu.do>
2026-06-13 19:43:04 -0400

    Correciones del ejercicio_1

--- src/Ejercicio_1/Main.java
+++ src/Ejercicio_1/Main.java
@@ -1,14 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        try{ 
         System.out.println("\n|| -- Simulacion de descarga de 3 archivos -- ||\n");
         MiHilo h = new MiHilo("Archivo1");
         MiHilo h2 = new MiHilo("Archivo2");
         MiHilo h3 = new MiHilo("Archivo3");
+
         System.out.println("Estado del hilo:" + h.getState());
         h.start();
         System.out.println("Estado del hilo:" + h2.getState());
         h2.start();
         System.out.println("Estado del hilo:" + h3.getState());
         h3.start();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
